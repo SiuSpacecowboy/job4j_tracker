3de7642e30969da8b39cf306bab6b77afc67b5aa
SiuSpacecowboy <danvitaly14@mail.ru>
2021-12-27 18:10:51 +0300

    1.1. Создание объекта [#165452]

---
+++ src/main/java/oop/Cat.java
@@ -0,0 +1,8 @@
+package oop;
+
+public class Cat {
+    public static void main(String[] args) {
+        Cat peppy = new Cat();
+        Cat sparky = new Cat();
+    }
+}
---
+++ src/main/java/oop/Dog.java
@@ -0,0 +1,11 @@
+package oop;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+        Dog sharik = new Dog();
+        Dog zhychka = new Dog();
+
+
+    }
+}
