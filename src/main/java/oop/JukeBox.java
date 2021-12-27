33d86dd58457c6aee7c8eb9cf7081daefa201468
SiuSpacecowboy <danvitaly14@mail.ru>
2021-12-27 18:43:17 +0300

    1.3. Вызов метода с аргументами. [#166086]

---
+++ src/main/java/oop/JukeBox.java
@@ -0,0 +1,26 @@
+package oop;
+
+public class JukeBox {
+
+    public void music(int position) {
+        if (position == 1) {
+            System.out.println("Пусть бегут неуклюже");
+        } else if (position == 2) {
+            System.out.println("Спокойной ночи");
+        } else {
+            System.out.println("Песня не найдена");
+        }
+    }
+
+    public static void main(String[] args) {
+        JukeBox car = new JukeBox();
+        int a = 1;
+        int b = 2;
+        int c = 0;
+        int c1 = 12;
+        car.music(a);
+        car.music(b);
+        car.music(c);
+        car.music(c1);
+    }
+}
