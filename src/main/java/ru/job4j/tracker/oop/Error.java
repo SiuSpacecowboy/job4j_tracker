e2289942e16d333fe388a9f9bbe0d9b6f7826505
SiuSpacecowboy <danvitaly14@mail.ru>
2021-12-29 00:55:26 +0300

    1.7. Конструктор [#173889 #244970]

--- src/main/java/ru/job4j/tracker/oop/Error.java
+++ src/main/java/ru/job4j/tracker/oop/Error.java
@@ -3,9 +3,7 @@ package ru.job4j.tracker.oop;
 public class Error {
 
     private boolean active;
-
     private  int status;
-
     private String message;
 
     public Error() {
@@ -17,10 +15,18 @@ public class Error {
         this.message = message;
     }
 
+    public void printnfo() {
+        System.out.println("Состояние " + active);
+        System.out.println("Статус " + status);
+        System.out.println("Вывод: " + message);
+    }
+
     public static void main(String[] args) {
         Error error = new Error();
         Error error1 = new Error(true, 1, "Access coplited");
         Error error2 = new Error(false, 0, "Access denied");
+        error.printnfo();
+        error1.printnfo();
+        error2.printnfo();
     }
-
 }
