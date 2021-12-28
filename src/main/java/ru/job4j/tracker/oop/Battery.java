05d23066b25adce3592f94d1727710517972d005
SiuSpacecowboy <danvitaly14@mail.ru>
2021-12-28 20:12:14 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/tracker/oop/Battery.java
@@ -0,0 +1,20 @@
+package ru.job4j.tracker.oop;
+
+public class Battery {
+
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
+    public void exchange(Battery another) {
+        another.load = another.load + this.load;
+        this.load = 0;
+    }
+}
+
