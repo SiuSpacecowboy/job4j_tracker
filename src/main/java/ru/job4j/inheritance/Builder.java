4327549477eabdec08e95b1a7d024b2c7eff649f
SiuSpacecowboy <danvitaly14@mail.ru>
2022-01-14 19:42:49 +0300

    3. Реализация профессий в коде [#6837 #250646]

--- src/main/java/ru/job4j/inheritance/Builder.java
+++ src/main/java/ru/job4j/inheritance/Builder.java
@@ -5,7 +5,7 @@ public class Builder extends Engineer {
     private String tool;
 
     public Builder(String name, String surname, String education, String birthday, String device, String tool) {
-        super (name, surname, education, birthday, device);
+        super(name, surname, education, birthday, device);
         this.tool = tool;
     }
 
