32280d08f782bca5c04fb5903560e3ba8a227c46
SiuSpacecowboy <danvitaly14@mail.ru>
2022-01-25 17:58:03 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -0,0 +1,20 @@
+package ru.job4j.pojo;
+
+public class Book {
+
+    private String name;
+    private int pages;
+
+    public Book(String name, int pages) {
+        this.name = name;
+        this.pages = pages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,37 @@
+package ru.job4j.pojo;
+
+public class Library {
+
+    public static void main(String[] args) {
+        Book book1 = new Book("Solaris", 322);
+        Book book2 = new Book("Great something", 425);
+        Book book3 = new Book("Dead souls", 333);
+        Book book4 = new Book("Clean code", 120);
+        Book[] library = new Book[4];
+        library[0] = book1;
+        library[1] = book2;
+        library[2] = book3;
+        library[3] = book4;
+        System.out.println("Library:");
+        for (int i = 0; i < library.length; i++) {
+            System.out.println("Name:" + library[i].getName()
+                    + " - " + library[i].getPages());
+        }
+        Book switchh = new Book("0", 0);
+        switchh = library[0];
+        library[0] = library[2];
+        library[2] = switchh;
+        System.out.println("Switch Library:");
+        for (int i = 0; i < library.length; i++) {
+            System.out.println("Name:" + library[i].getName()
+                    + " - " + library[i].getPages());
+        }
+        System.out.println("Just Clean code");
+        for (int i = 0; i < library.length; i++) {
+            if (library[i].getName() == "Clean code") {
+                System.out.println("Name:" + library[i].getName()
+                        + " - " + library[i].getPages());
+            }
+        }
+    }
+}
