5a9584c4b40fb756a76cfb47c015c30e342451dd
SiuSpacecowboy <danvitaly14@mail.ru>
2022-01-25 20:25:35 +0300

    3.1. Массив с пустыми ячейками [#176470]

---
+++ src/main/java/ru/job4j/pojo/Product.java
@@ -0,0 +1,19 @@
+package ru.job4j.pojo;
+
+public class Product {
+    private String name;
+    private int count;
+
+    public Product(String name, int count) {
+        this.name = name;
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
