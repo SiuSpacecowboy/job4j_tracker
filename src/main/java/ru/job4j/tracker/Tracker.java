01d035e01477ab84005ae805e3f098d86a698261
SiuSpacecowboy <danvitaly14@mail.ru>
2022-01-26 19:08:01 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,34 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+       Item[] finarr = new Item[size];
+       int count = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = items[i];
+            if (item != null) {
+                finarr[count] = item;
+                count++;
+            }
+        }
+        finarr = Arrays.copyOf(finarr, count);
+        return finarr;
+    }
+
+        public Item[] findByName(String key) {
+        Item[] res = new Item[size];
+        int count = 0;
+            for (int i = 0; i < size; i++) {
+                Item item = items[i];
+                if (item.getName() == key) {
+                    res[count] = item;
+                    count++;
+                }
+            }
+            res = Arrays.copyOf(res, count);
+            return res;
     }
 
     public Item findById(int id) {
