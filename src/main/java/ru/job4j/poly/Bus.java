6a38db9ad2c7af33dda6301c10acb5f1357ff635
SiuSpacecowboy <danvitaly14@mail.ru>
2022-01-29 19:37:59 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,20 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+
+    @Override
+    public void drive() {
+    }
+
+    @Override
+    public void passengers(int count) {
+        int pass = 15;
+    }
+
+    @Override
+    public double refuel(double fuel) {
+        double luckoil = 12.5;
+        double refuel = luckoil * 50;
+        return refuel;
+    }
+}
