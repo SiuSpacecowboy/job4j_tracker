c00e6bdb786ecdeb6267da8fe71ddcf87066435b
SiuSpacecowboy <danvitaly14@mail.ru>
2022-02-28 17:07:39 +0300

    0.4. Пользовательские исключения. [#219367 #270094]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,8 +4,9 @@ public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
         for (int index = 0; index < value.length; index++) {
-            if (key == value[index]) {
+            if (key.equals(value[index])) {
                 rsl = index;
+                break;
             }
         }
         if (rsl == -1) {
