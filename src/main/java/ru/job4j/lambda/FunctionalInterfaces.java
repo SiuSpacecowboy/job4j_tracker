830d65ee53db540932b153bf94eda20940e7a446
SiUSpacecowboy <danvitaly14@mail.ru>
2023-01-18 17:03:46 +0300

    2.2. Функциональный интерфейс [#247497]

--- src/main/java/ru/job4j/lambda/FunctionalInterfaces.java
+++ src/main/java/ru/job4j/lambda/FunctionalInterfaces.java
@@ -17,17 +17,14 @@ public class FunctionalInterfaces {
         biCon.accept(5, "five");
         biCon.accept(6, "six");
         biCon.accept(7, "seven");
-
         BiPredicate<Integer, String> biPred = (i, s) -> i % 2 == 0 || s.length() == 4;
         for (Integer i : map.keySet()) {
             if (biPred.test(i, map.get(i))) {
                 System.out.println("key: " + i + " value: " + map.get(i));
             }
         }
-
         Supplier<List<String>> sup = () -> new ArrayList<>(map.values());
         List<String> strings = sup.get();
-
         Consumer<String> con = (s) -> System.out.println(s);
         Function<String, String> func = s -> s.toUpperCase();
         for (String s : strings) {
