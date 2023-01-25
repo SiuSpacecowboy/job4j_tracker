49355b4f9f9ac17fa3ef5f334c07a4d8b16339f3
SiUSpacecowboy <danvitaly14@mail.ru>
2023-01-25 18:35:28 +0300

    7. Уникальность элементов и сортировка. [#110225]

--- src/main/java/ru/job4j/stream/Address.java
+++ src/main/java/ru/job4j/stream/Address.java
@@ -35,5 +35,10 @@ public class Address {
     public int hashCode() {
         return Objects.hash(city, street, home, apartment);
     }
+
+    public String getCity() {
+        return city;
+    }
+
 }
 
