9f73019ac45c7a7201fbb508917e59a47d0a75be
SiUSpacecowboy <danvitaly14@mail.ru>
2023-01-26 16:50:12 +0300

    9. Генерирование колоды карт. FlatMap [#504873 #385052]

--- src/main/java/ru/job4j/stream/Card.java
+++ src/main/java/ru/job4j/stream/Card.java
@@ -21,10 +21,10 @@ public class Card {
 
     @Override
     public String toString() {
-        return "Card{" +
-                "suit=" + suit +
-                ", value=" + value +
-                '}';
+        return "Card{"
+                + "suit=" + suit
+                + ", value=" + value
+                + '}';
     }
 
     public static void main(String[] args) {
