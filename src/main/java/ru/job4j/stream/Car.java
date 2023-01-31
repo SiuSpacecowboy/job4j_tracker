46e480754d66bef343887059203580a72f07d1b7
SiUSpacecowboy <danvitaly14@mail.ru>
2023-01-31 21:32:31 +0300

    18. Шаблоны проектирования в Stream API [#310012]

--- src/main/java/ru/job4j/stream/Car.java
+++ src/main/java/ru/job4j/stream/Car.java
@@ -11,8 +11,8 @@ public class Car {
 
     @Override
     public String toString() {
-        return "Car{" +
-                "brand='" + brand + '\''
+        return "Car{"
+                + "brand='" + brand + '\''
                 + ", model='" + model + '\''
                 + ", created=" + created
                 + ", volume=" + volume
