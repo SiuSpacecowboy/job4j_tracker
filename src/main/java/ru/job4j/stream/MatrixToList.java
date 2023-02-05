73822fd22441068c712f8587d76421f251ce5465
SiUSpacecowboy <danvitaly14@mail.ru>
2023-02-05 16:15:00 +0300

    0. Stream API улучшения [#58167]

--- src/main/java/ru/job4j/stream/MatrixToList.java
+++ src/main/java/ru/job4j/stream/MatrixToList.java
@@ -10,6 +10,6 @@ public class MatrixToList {
     public static List<Integer> convert(Integer[][] matrix) {
         return Stream.of(matrix)
                 .flatMap(Arrays::stream)
-                .collect(Collectors.toList());
+                .toList();
     }
 }
