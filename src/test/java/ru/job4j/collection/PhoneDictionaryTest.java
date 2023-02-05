df2acae8e7550095e955432a8e850ff54861ef38
SiUSpacecowboy <danvitaly14@mail.ru>
2023-02-05 19:23:48 +0300

    2. Local-Variable Type Inference [#70626]

--- src/test/java/ru/job4j/collection/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/collection/PhoneDictionaryTest.java
@@ -11,21 +11,21 @@ public class PhoneDictionaryTest {
 
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Petr");
+        var persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname(), is("Arsentev"));
     }
 
     @Test
     public void whenNotFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("DEm");
+        var persons = phones.find("DEm");
         assertTrue(persons.isEmpty());
     }
 }
--- src/test/java/ru/job4j/collection/PriorityQueueTest.java
+++ src/test/java/ru/job4j/collection/PriorityQueueTest.java
@@ -9,11 +9,11 @@ public class PriorityQueueTest {
 
     @Test
     public void whenHigherPriority() {
-        PriorityQueue queue = new PriorityQueue();
+        var queue = new PriorityQueue();
         queue.put(new Task("low", 5));
         queue.put(new Task("urgent", 1));
         queue.put(new Task("middle", 3));
-        Task result = queue.take();
+        var result = queue.take();
         assertThat(result.getDesc(), is("urgent"));
     }
 }
