6cc3939aad21498bdbe0611d22face3b8ed5865c
SiUSpacecowboy <danvitaly14@mail.ru>
2023-02-05 19:28:51 +0300

    0. Stream API улучшения [#58167 #388554]

--- src/main/java/ru/job4j/stream/StudentLevel.java
+++ src/main/java/ru/job4j/stream/StudentLevel.java
@@ -9,7 +9,7 @@ public class StudentLevel {
 
     public static List<Student> levelOf(List<Student> students, int bound) {
         return students.stream()
-                .flatMap(Stream::ofNullable)
+                .filter(Objects::nonNull)
                 .sorted((left, right) -> Integer.compare(right.getScore(), left.getScore()))
                 .takeWhile(st -> st.getScore() > bound)
                 .collect(Collectors.toList());
